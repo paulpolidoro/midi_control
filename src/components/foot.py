1dc1555c9561dfa82ae3dafcb97da959962ee6a7
Paul Polidoro <leedshs@gmail.com>
2025-04-08 15:47:04 -0300

    Update foot.py

--- src/components/foot.py
+++ src/components/foot.py
@@ -1,94 +1,85 @@
 import threading
-from typing import Callable
+import time
+from typing import Callable, Optional
 
 import RPi.GPIO as GPIO
-import time
 
 from src.components.pin import Pin
 
 
 class Foot(Pin):
-    """
-    Classe para controle dos footswitchs
-    """
     LONG_PRESS_THRESHOLD = 2
 
-    def __init__(self, pin, name: str = '',
-                 on_press: Callable[[str], None] = None,
-                 on_press_2: Callable[[str], None] = None,
-                 on_press_3: Callable[[str], None] = None,
-                 on_release: Callable[[str, float], None] = None):
-        """
-        Classe Foot representa um footswitch com várias ações de pressão.
-
-        :param pin: O número do pino GPIO ao qual o footswitch está conectado.
-        :param name: O nome do footswitch para identificação.
-        :param on_press: Função a ser chamada quando o footswitch é pressionado.
-        :param on_press_2: Função a ser chamada quando o footswitch é pressionado por um curto período.
-        :param on_press_3: Função a ser chamada quando o footswitch é pressionado por um longo período.
-        :param on_release: Função a ser chamada quando o footswitch é liberado, recebendo a duração da pressão como argumento.
-        """
+    def __init__(self, pin:int, controller, name: Optional[str] = None):
         super().__init__(pin)
-
         self._name = name
-
-        self.on_press = on_press
-        self.on_press_2 = on_press_2
-        self.on_press_3 = on_press_3
-        self.on_release = on_release
-        self.press_time = None
+        self._controller = controller
         self.is_pressed = False
-        self.long_press_triggered = False
-        self.running = True
+        self._press_time = 0
+        self._disable_callback = False
+        self._monitoring = False
+
+        self.on_press:Optional[Callable[[str], None]] = None
+        self.on_short_press:Optional[Callable[[str], None]] = None
+        self.on_long_press:Optional[Callable[[str], None]] = None
+        self.on_release:Optional[Callable[[str], None]] = None
 
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self._pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.add_event_detect(self._pin, GPIO.BOTH, callback=self._handle_press)
+
+    def _handle_press(self, channel):
+        if GPIO.input(self._pin) == GPIO.LOW:
+            self._start_monitoring()
+            self._press_time = time.time()
+            self.is_pressed = True
+
+            if not self._disable_callback and not self._controller.is_processing_multiple_press():
+                self.on_press(self._name)
+        elif GPIO.input(self._pin) == GPIO.HIGH:
+            if (time.time() - self._press_time) < self.LONG_PRESS_THRESHOLD:
+                if not self._disable_callback and not self._controller.is_processing_multiple_press():
+                    self.on_short_press(self._name)
+
+            self._stop_monitoring()
+            self.is_pressed = False
+            self._press_time = 0
+
+            if not self._disable_callback and not self._controller.is_processing_multiple_press():
+                self.on_release(self._name)
+
+    def _monitor_press(self):
+        self._monitoring = True
+
+        while self._monitoring:
+            if self.is_pressed:
+                if (time.time() - self._press_time) >= self.LONG_PRESS_THRESHOLD:
+                    if not self._disable_callback and not self._controller.is_processing_multiple_press():
+                        self.on_long_press(self._name)
+            time.sleep(0.1)
+
+    def _start_monitoring(self):
         self.monitor_thread = threading.Thread(target=self._monitor_press)
         self.monitor_thread.start()
 
-    def _handle_press(self, channel):
-        if GPIO.input(self._pin) == GPIO.LOW and not self.is_pressed:
-            self.press_time = time.time()
-            self.is_pressed = True
-            self.long_press_triggered = False
-
-            if self.on_press:
-                self.on_press(self._name)
-        elif GPIO.input(self._pin) == GPIO.HIGH and self.is_pressed:
-            press_duration = time.time() - self.press_time
-            if press_duration < self.LONG_PRESS_THRESHOLD:
-                if self.on_press_2:
-                    self.on_press_2(self._name)
-            if self.on_release:
-                self.on_release(self._name, press_duration)
-
-            self.is_pressed = False
-            self.press_time = None
-
-    def _monitor_press(self):
-        while self.running:
-            if self.is_pressed:
-                press_duration = time.time() - self.press_time
-                if press_duration >= self.LONG_PRESS_THRESHOLD and not self.long_press_triggered:
-                    if self.on_press_3:
-                        self.on_press_3(self._name)
-                    self.long_press_triggered = True
-            time.sleep(0.1)
+    def _stop_monitoring(self):
+        self._monitoring = False
+        self.monitor_thread.join()
 
     def is_pressed(self):
         return self.is_pressed
 
-    def get_duration(self):
-        if self.is_pressed:
-            return time.time() - self.press_time
-        else:
-            return 0
-
-    def get_name(self):
+    def get_name(self)->str:
         return self._name
 
-    def cleanup(self):
-        self.running = False
-        self.monitor_thread.join()
-        super().cleanup()
+    def get_press_time(self)->float:
+        return self._press_time
+
+    def disable_callback(self):
+        self._disable_callback = True
+
+    def enable_callback(self):
+        self._disable_callback = False
+
+    def callback_state(self)->bool:
+        return not self._disable_callback
