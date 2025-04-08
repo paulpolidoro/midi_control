9f2077ab7f2d8ad22cc5a8f9bd5dfbccc1744078
Paul Polidoro <leedshs@gmail.com>
2025-04-08 16:08:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
 import time
+from typing import List
 
 import RPi.GPIO as GPIO
 
 from src.components.tap import Tap
+from src.controllers.foot_controller import FootController
+from src.controllers.program_change import ProgramChange
 from src.hardware import pins
 from src.components.foot import Foot
 from src.components.led import Led
+
+controller: FootController = FootController([])
+foots: List[Foot] = []
+tap: Tap = Tap(pins.LED_TAP)
+
+def start_components():
+    for i in range(4):
+        foots.append(Foot(getattr(pins, f"FOOT_{i}"), controller, f"ft{i}"))
+
+    controller.foots.extend(foots)
+    controller.start_monitoring()
 
 def main():
     led_power = Led(pins.LED_POWER)
     led_power.on()
 
-    tap = Tap(Led(pins.LED_TAP))
+    program_change = ProgramChange(banks=10)
 
-    foot_ctl = Foot(pins.FOOT_4, on_press=tap.tap)
+    foots[0].on_press = program_change.set_preset_by_foot(foots[0].get_name())
+    foots[2].on_press = program_change.set_preset_by_foot(foots[1].get_name())
+    foots[2].on_press = program_change.set_preset_by_foot(foots[2].get_name())
+    foots[4].on_press = tap.tap()
 
     try:
         while True:
@@ -25,4 +42,6 @@ def main():
         GPIO.cleanup()
 
 if __name__ =="__main__":
+    start_components()
     main()
+    
